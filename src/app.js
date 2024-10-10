f41397bcd965f83d4f2a08d1589fb6df6e778828
Juancolque04 <jdiazcolque04@gmail.com>
2024-10-09 23:01:32 -0300

    json

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ const __dirname = dirname(__filename);
 const app = express();
 
 const whitelist = ['http://127.0.0.1:5501', 'https://desarrollo-social.vercel.app'];
+
 const corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
