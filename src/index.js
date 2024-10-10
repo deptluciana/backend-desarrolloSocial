39d7677c84f48813cde49f509c32c86d69fc9f0a
Juancolque04 <jdiazcolque04@gmail.com>
2024-10-10 00:32:30 -0300

    cambio 6

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { connectDB } from "./db.js";
 
 async function main() {
   await connectDB();
-  app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  
+  // Asegúrate de escuchar en todas las interfaces (0.0.0.0) y en el puerto correcto
+  app.listen(PORT || 4486, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
   });
-  
 }
-  
+
 main();
