a3729f7901851b4177da873db880b8bdb71bd732
Juancolque04 <jdiazcolque04@gmail.com>
2024-10-22 00:14:10 -0300

    cambios 4

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -3,22 +3,27 @@ import { Router } from 'express';
 import multer from 'multer';
 import { uploadFile, getFilesBySection, deleteFile} from '../controllers/file.controllers.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js'
-import path from 'path';
+import fs from 'fs';
 
 const router = Router();
 
-const __dirname = path.resolve(); 
 // Configuración de multer para manejar la subida de archivos
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, 'uploads/')); // Usa una ruta absoluta
+        const uploadPath = 'uploads/';
+
+        // Verificar si la carpeta 'uploads' existe, y si no, crearla
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath); // Carpeta donde se almacenarán los archivos
     },
     filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + '-' + file.originalname);
+        cb(null, uniqueSuffix + '-' + file.originalname); // Nombre único para evitar colisiones
     },
 });
-
 
 const upload = multer({ storage });
 
